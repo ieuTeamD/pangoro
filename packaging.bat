37a279437ba0cae8e0daf1a717f278614a25054f
ieuTeamD <100965835+ieuTeamD@users.noreply.github.com>
2022-03-06 00:03:34 +0300

    fixes

--- packaging.bat
+++ packaging.bat
@@ -1,9 +1,9 @@
 cd "C:\Users\khalh\Desktop\APY\GroupProject\pangoro"
-py -m pip install --upgrade build
+python -m pip install --upgrade build
 
 python setup.py sdist
-py -m twine upload --repository pypi dist/*
+python -m twine upload --repository pypi dist/*
 
 pip install --upgrade pangoro
 
-#py -m twine upload --repository testpypi dist/*
+REM python -m twine upload --repository testpypi dist/*
