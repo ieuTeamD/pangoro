91003ea622c25ba1dd64efbf3d771a9ef7c401d4
ieuTeamD <100965835+ieuTeamD@users.noreply.github.com>
2022-03-09 00:42:41 +0300

    update release and libraries required

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='pangoro',
-    version='0.21',
+    version='0.28',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='pangoro python package',
     long_description=open('README.txt').read(),
-    install_requires=['numpy','pandas','sklearn'],
+    install_requires=['numpy','pandas','sklearn','seaborn','matplotlib'],
     url='https://github.com/ieuTeamD',
     author='Team D',
     author_email='ieuTeamD@gmail.com'
