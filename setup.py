e07d49fed3e0e08dad2d4530435f419cbd0e9dba
ieuTeamD <100965835+ieuTeamD@users.noreply.github.com>
2022-03-09 23:24:35 +0300

    updated markdown config

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='pangoro',
-    version='0.30',
+    version='0.32',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='pangoro python package',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+
     install_requires=['numpy','pandas','sklearn','seaborn','matplotlib'],
     url='https://github.com/ieuTeamD',
     author='Team D',
